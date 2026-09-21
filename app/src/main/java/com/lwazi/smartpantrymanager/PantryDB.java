06d461619cc8f49d39eedd6cb9be1e937f4d8ef1
Lwazi Ndwandwe <successndwandwe5@icloud.com>
2026-09-21 20:28:18 +0200

    Added PantryDB SQLITEOpenHelper class

---
+++ app/src/main/java/com/lwazi/smartpantrymanager/PantryDB.java
@@ -0,0 +1,38 @@
+package com.lwazi.smartpantrymanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PantryDB extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "SmartPantryManager.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_INGREDIENTS = "ingredients";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_QUANTITY = "quantity";
+    public static final String COLUMN_UNIT = "unit";
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_INGREDIENTS + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_NAME + " TEXT NOT NULL, " +
+                    COLUMN_QUANTITY + " REAL NOT NULL, " +
+                    COLUMN_UNIT + " TEXT NOT NULL);";
+
+    public PantryDB(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_INGREDIENTS);
+        onCreate(db);
+    }
+
+}
