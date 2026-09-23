7bef69289521c4889454ccb7864bbb5ed58a69ca
Lwazi Ndwandwe <successndwandwe5@icloud.com>
2026-09-23 22:12:12 +0200

    Added recipes and recipe_ingredients tables

--- app/src/main/java/com/lwazi/smartpantrymanager/PantryDB.java
+++ app/src/main/java/com/lwazi/smartpantrymanager/PantryDB.java
@@ -15,11 +15,43 @@ public class PantryDB extends SQLiteOpenHelper {
     public static final String COLUMN_QUANTITY = "quantity";
     public static final String COLUMN_UNIT = "unit";
 
+    //Recipes table
+    public static final String TABLE_RECIPES = "recipes";
+    public static final String COLUMN_RECIPE_ID = "id";
+    public static final String COLUMN_RECIPE_NAME = "name";
+    public static final String COLUMN_INSTRUCTIONS  = "instructions";
+
+    //RecipeIngredients table
+    public static final String TABLE_RECIPE_INGREDIENTS = "recipe_ingredients";
+    public static final String COLUMN_RI_ID = "id"; // this row's own primary key
+    public static final String COLUMN_RI_RECIPE_ID = "recipe_id"; // which recipe this ingredient requirement belongs to
+    public static final String COLUMN_RI_INGREDIENT_NAME = "ingredient_name";
+    public static final String COLUMN_RI_QUANTITY_REQUIRED = "quantity_required";
+    public static final String COLUMN_RI_UNIT = "unit";
+
+
     private static final String TABLE_CREATE =
             "CREATE TABLE " + TABLE_INGREDIENTS + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     COLUMN_NAME + " TEXT NOT NULL, " +
                     COLUMN_QUANTITY + " REAL NOT NULL, " +
                     COLUMN_UNIT + " TEXT NOT NULL);";
+
+    //SQL query creating recipe table
+    private static final String RECIPE_TABLE_CREATE =
+            "CREATE TABLE " + TABLE_RECIPES + "(" + COLUMN_RECIPE_ID+ " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_RECIPE_NAME + " TEXT NOT NULL, " +
+                    COLUMN_INSTRUCTIONS + " TEXT);";
+
+    //Sql query creating recipe ingredients table
+    private static final String RECIPE_INGREDIENTS_TABLE_CREATE =
+            "CREATE TABLE " + TABLE_RECIPE_INGREDIENTS + "(" +
+                    COLUMN_RI_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_RI_RECIPE_ID + " INTEGER NOT NULL, " +
+                    COLUMN_RI_INGREDIENT_NAME + " TEXT NOT NULL, " +
+                    COLUMN_RI_QUANTITY_REQUIRED + " REAL NOT NULL, " +
+                    COLUMN_RI_UNIT + " TEXT NOT NULL, " +
+                    "FOREIGN KEY(" + COLUMN_RI_RECIPE_ID + ") REFERENCES " +
+                    TABLE_RECIPES + "(" + COLUMN_RECIPE_ID + "));";
 
     public PantryDB(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,11 +59,15 @@ public class PantryDB extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(TABLE_CREATE);
+        db.execSQL(TABLE_CREATE);//creates the ingredients table
+        db.execSQL(RECIPE_TABLE_CREATE);//creates recipe table
+        db.execSQL(RECIPE_INGREDIENTS_TABLE_CREATE);//creates the recipe ingredients table
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_INGREDIENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RECIPES);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RECIPE_INGREDIENTS);
         onCreate(db);
     }
 
