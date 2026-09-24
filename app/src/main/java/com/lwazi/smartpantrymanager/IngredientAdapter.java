042ae0f1b6e14355e48aab7049e54296d5ad2d3c
Lwazi Ndwandwe <successndwandwe5@icloud.com>
2026-09-24 21:46:48 +0200

    Added IngredientAdapter to bind ingredient data to RecyclerView

---
+++ app/src/main/java/com/lwazi/smartpantrymanager/IngredientAdapter.java
@@ -0,0 +1,60 @@
+package com.lwazi.smartpantrymanager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.IngredientViewHolder> {
+
+    private List<Ingredient> ingredientList;
+
+    public IngredientAdapter(List<Ingredient> ingredientList) {
+        this.ingredientList = ingredientList;
+    }
+
+    //this is called when the RecyclerView needs a brand new empty row
+    @NonNull
+    @Override
+    public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_ingredient, parent, false); //turns item_ingredient.xml into a View
+        return new IngredientViewHolder(view);
+    }
+
+    //this is called to fill a row with a specific ingredient's data
+    @Override
+    public void onBindViewHolder(@NonNull IngredientViewHolder holder, int position) {
+        Ingredient ingredient = ingredientList.get(position);
+        holder.textName.setText(ingredient.getName());
+        //combines quantity and unit into one readable string
+        holder.textQuantity.setText(ingredient.getQuantity() + " " + ingredient.getUnit());
+    }
+
+    //this tells Android how many rows exist in total
+    @Override
+    public int getItemCount() {
+        return ingredientList.size();
+    }
+
+    //calls this when the data changes to refresh what's on screen
+    public void updateIngredients(List<Ingredient> newIngredients) {
+        this.ingredientList = newIngredients;
+        notifyDataSetChanged(); //this tells the RecyclerView to redraw itself with the new data
+    }
+
+    //holds references to the two TextViews inside one row of item_ingredient.xml
+    static class IngredientViewHolder extends RecyclerView.ViewHolder {
+        TextView textName;
+        TextView textQuantity;
+
+        public IngredientViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textName = itemView.findViewById(R.id.textIngredientName);
+            textQuantity = itemView.findViewById(R.id.textIngredientQuantity);
+        }
+    }
+}
